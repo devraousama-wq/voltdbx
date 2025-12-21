999f11b390f75018654431568b3cac4c4ec5b2a9
devraousama-wq <286299166+devraousama-wq@users.noreply.github.com>
2025-12-21 22:11:00 +0500

    Wire GET and SET commands through dispatcher into storage engine

--- src/protocol/dispatcher.cpp
+++ src/protocol/dispatcher.cpp
@@ -13,6 +13,28 @@ void CommandDispatcher::register_builtin_handlers() {
     handlers_[CommandType::Info] = [](const ParsedCommand&) {
         return "+OK voltdbx/0.1";
     };
+    handlers_[CommandType::Get] = [this](const ParsedCommand& cmd) {
+        if (cmd.args.size() != 1) {
+            return std::string("-ERR wrong number of arguments for GET");
+        }
+        auto value = storage_.get(cmd.args[0]);
+        if (!value) {
+            return std::string("$-1");
+        }
+        return std::string("$") + std::to_string(value->size()) + "\r\n" + *value;
+    };
+    handlers_[CommandType::Set] = [this](const ParsedCommand& cmd) {
+        if (cmd.args.size() < 2) {
+            return std::string("-ERR wrong number of arguments for SET");
+        }
+        std::string value = cmd.args[1];
+        for (std::size_t i = 2; i < cmd.args.size(); ++i) {
+            value.push_back(' ');
+            value += cmd.args[i];
+        }
+        storage_.set(cmd.args[0], value);
+        return std::string("+OK");
+    };
 }
 
 std::string CommandDispatcher::dispatch(const ParsedCommand& cmd) {
